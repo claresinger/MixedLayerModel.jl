b377c4d4a1bc1e535b82ed1a7565ce6b4158278d
claresinger <csinger@caltech.edu>
2020-10-16 19:39:54 -0700

    fix conversion from python to julia finally

--- plotting/MakeDiagram.jl
+++ plotting/MakeDiagram.jl
@@ -1,36 +1,39 @@
-using PyPlots
+using PyPlot
 using LaTeXStrings
 
-function make_MLM_diagram()
-    fig = figure(figsize=(8,6))
-    rcParams.update({'font.size': 15})
+fig = figure(figsize=(8,6));
+rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams");
+rcParams["font.size"] = 15;
 
-    plot([0,80],[0,0],"k-")
-    fill_between([0,80],[50,50],[100,100],color="grey",alpha=0.5)
-    plot([20,20,10,10],[0,100,102,150],"k-")
-    plot([65,65,50,45],[0,100,102,150],"k-")
-    
-    RH = concatenate((58 + linspace(0,7,num=50), 65 + zeros(50), 35 - linspace(0,2,num=5)))
-    z = concatenate((linspace(0,100,num=100), linspace(102,150,num=5)))
-    plot(RH,z,"k:")
+plot([0,80],[0,0],"k-");
+fill_between([0,80],[50,50],[100,100],color="grey",alpha=0.5);
+plot([20,20,10,10],[0,100,102,150],"k-");
+plot([65,65,50,45],[0,100,102,150],"k-");
 
-    text(-4,50,"$z_b$")
-    text(-4,100,"$z_i$")
+RH1 = 58 .+ collect(range(0,7,length=50));
+RH2 = 65 .+ zeros(50);
+RH3 = 35 .- collect(range(0,2,length=5));
+RH = vcat(RH1..., RH2..., RH3...);
 
-    text(66,10,"$q_{tM}$")
-    text(21,10,"$h_M$")
-    text(51,10,"RH(z)")
-    
-    text(11,120,"$h^{ft}(z)$")
-    text(49,120,"$q_t^{ft}(z)$")
-    text(35,120,"RH$^{ft}$")
+z1 = collect(range(0,100,length=100));
+z2 = collect(range(102,150,length=5));
+z = vcat(z1..., z2...);
 
-    #title("Mixed-layer model schematic",fontsize=15)
-    axis("off")
-    box(False)
+plot(RH,z,"k:");
 
-    tight_layout()
-    savefig("./figs/mlm-diagram.png",dpi=300)
-    #show()
+text(-4,50,L"$z_b$");
+text(-4,100,L"$z_i$");
 
-end
+text(66,10,L"$q_{tM}$");
+text(21,10,L"$h_M$");
+text(51,10,L"RH(z)");
+
+text(11,120,L"$h^{ft}(z)$");
+text(49,120,L"$q_t^{ft}(z)$");
+text(35,120,L"RH$^{ft}$");
+
+axis("off");
+box(false);
+
+tight_layout();
+savefig("plotting/figures/mlm-diagram.png",dpi=300);

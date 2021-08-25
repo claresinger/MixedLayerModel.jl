13e78764da4337c0d944b16779f8932ba271ad72
claresinger <clareees@gmail.com>
2021-08-25 10:31:47 -0700

    more experiments

--- experiments/mlm_solve_funcs.jl
+++ experiments/mlm_solve_funcs.jl
@@ -1,10 +1,10 @@
 """
-    run_mlm(params, filename="default.txt")
+    run_mlm(params; dt=x, tspan=(0.0,x))
 
     run MLM simulation with given parameters
     and save output to file
 """
-function run_mlm(params, dt=3600.0*5.0, tspan=(0.0,3600.0*24.0*10.0))
+function run_mlm(params; dt=3600.0*5.0, tspan=(0.0,3600.0*24.0*10.0))
     params.qft0 = calc_qft0(params.RHft, params.Gamma_q, params.sft0, params.Gamma_s);
     
     z0 = 0.0;
@@ -17,38 +17,38 @@ function run_mlm(params, dt=3600.0*5.0, tspan=(0.0,3600.0*24.0*10.0))
     prob = ODEProblem(mlm, u0, tspan, params);
 
     @time begin
-        sol = solve(prob, Euler(), dt=dt, progress=true, progress_steps=10);
+        sol = solve(prob, Euler(), dt=dt, progress=true, progress_steps=50);
     end
 
     return u0, sol
 end
 
 """
-    run_mlm_from_init(u0, params, filename="default.txt")
+    run_mlm_from_init(u0, params; dt=x, tspan=(0.0,x))
 
     run MLM simulation with given parameters
     from initial state u0
     and save output to file
 """
-function run_mlm_from_init(u0, params, dt=3600.0*5.0, tspan=(0.0,3600.0*24.0*10.0), filename="default.txt")
+function run_mlm_from_init(u0, params; dt=3600.0*5.0, tspan=(0.0,3600.0*24.0*10.0))
     params.qft0 = calc_qft0(params.RHft, params.Gamma_q, params.sft0, params.Gamma_s);
 
     prob = ODEProblem(mlm, u0, tspan, params);
 
     @time begin
-        sol = solve(prob, Euler(), dt=dt, progress=true, progress_steps=10);
+        sol = solve(prob, Euler(), dt=dt, progress=true, progress_steps=50);
     end
 
     return u0, sol
 end
 
 """
-    run_mlm_ss(params, filename="default.txt")
+    run_mlm_ss(params; dt=x, tspan=x)
 
     run MLM simulation with given parameters
     and save output to file
 """
-function run_mlm_ss(params, dt=3600.0*5.0, tspan=3600.0*24.0*10.0)
+function run_mlm_ss(params; dt=3600.0*5.0, tspan=3600.0*24.0*10.0)
     params.qft0 = calc_qft0(params.RHft, params.Gamma_q, params.sft0, params.Gamma_s);
     
     z0 = 0.0;
@@ -59,7 +59,6 @@ function run_mlm_ss(params, dt=3600.0*5.0, tspan=3600.0*24.0*10.0)
     u0 = [zi0, hM0, qtM0, params.SST0];
 
     prob = SteadyStateProblem(mlm, u0, params);
-    #tspan = 3600.0 * 24.0 * 30.0;
     tol = 1e-9;
 
     @time begin
@@ -67,7 +66,7 @@ function run_mlm_ss(params, dt=3600.0*5.0, tspan=3600.0*24.0*10.0)
         # sol = solve(prob, SSRootfind());
 
         println("euler, dt=4 hrs");
-        sol = solve(prob, DynamicSS(Euler(); abstol=0.0, reltol=tol, tspan=tspan), dt=dt, progress=true, progress_steps=30);
+        sol = solve(prob, DynamicSS(Euler(); abstol=0.0, reltol=tol, tspan=tspan), dt=dt, progress=true, progress_steps=50);
 
         # println("tsit5");
         # sol = solve(prob,DynamicSS(Tsit5()))
@@ -86,13 +85,13 @@ function run_mlm_ss(params, dt=3600.0*5.0, tspan=3600.0*24.0*10.0)
 end
 
 """
-    run_mlm_ss_from_init(u0, params, filename="default.txt")
+    run_mlm_ss_from_init(u0, params; dt=x, tspan=x)
 
     run MLM simulation with given parameters
     from initial state u0
     and save output to file
 """
-function run_mlm_ss_from_init(u0, params, dt=3600.0*5.0, tspan=3600.0*24.0*10.0, filename="default.txt")
+function run_mlm_ss_from_init(u0, params; dt=3600.0*5.0, tspan=3600.0*24.0*10.0)
     params.qft0 = calc_qft0(params.RHft, params.Gamma_q, params.sft0, params.Gamma_s);
 
     prob = SteadyStateProblem(mlm, u0, params);
@@ -103,7 +102,7 @@ function run_mlm_ss_from_init(u0, params, dt=3600.0*5.0, tspan=3600.0*24.0*10.0,
         # sol = solve(prob, SSRootfind());
 
         println("euler");
-        sol = solve(prob, DynamicSS(Euler(); abstol=0.0, reltol=tol, tspan=tspan), dt=dt, progress=true, progress_steps=30);
+        sol = solve(prob, DynamicSS(Euler(); abstol=0.0, reltol=tol, tspan=tspan), dt=dt, progress=true, progress_steps=50);
 
         # println("tsit5");
         # sol = solve(prob, DynamicSS(Tsit5(); abstol=0.0, reltol=tol, tspan=tspan));
@@ -112,7 +111,7 @@ function run_mlm_ss_from_init(u0, params, dt=3600.0*5.0, tspan=3600.0*24.0*10.0,
         # sol = solve(prob, DynamicSS(Rosenbrock23(autodiff=false); abstol=0.0, reltol=tol, tspan=tspan));
         
         # println("cvode_bdf");
-        # sol = solve(prob, DynamicSS(CVODE_BDF(); abstol=0.0, reltol=tol), dt=3600.0, progress=true, progress_steps=30);
+        # sol = solve(prob, DynamicSS(CVODE_BDF(); abstol=0.0, reltol=tol), dt=3600.0, progress=true, progress_steps=50);
     end
 
     return u0, sol

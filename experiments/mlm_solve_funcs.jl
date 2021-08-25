1009db5b2c85cb9c0337229be3c850d747aaf868
claresinger <clareees@gmail.com>
2021-08-24 22:57:56 -0700

    run some experiments

--- experiments/mlm_solve_funcs.jl
+++ experiments/mlm_solve_funcs.jl
@@ -48,7 +48,7 @@ end
     run MLM simulation with given parameters
     and save output to file
 """
-function run_mlm_ss(params)
+function run_mlm_ss(params, dt=3600.0*5.0, tspan=3600.0*24.0*10.0)
     params.qft0 = calc_qft0(params.RHft, params.Gamma_q, params.sft0, params.Gamma_s);
     
     z0 = 0.0;
@@ -59,7 +59,7 @@ function run_mlm_ss(params)
     u0 = [zi0, hM0, qtM0, params.SST0];
 
     prob = SteadyStateProblem(mlm, u0, params);
-    tspan = 3600.0 * 24.0 * 30.0;
+    #tspan = 3600.0 * 24.0 * 30.0;
     tol = 1e-9;
 
     @time begin
@@ -67,7 +67,7 @@ function run_mlm_ss(params)
         # sol = solve(prob, SSRootfind());
 
         println("euler, dt=4 hrs");
-        sol = solve(prob, DynamicSS(Euler(); abstol=0.0, reltol=tol, tspan=tspan), dt=3600.0*4, progress=true, progress_steps=30);
+        sol = solve(prob, DynamicSS(Euler(); abstol=0.0, reltol=tol, tspan=tspan), dt=dt, progress=true, progress_steps=30);
 
         # println("tsit5");
         # sol = solve(prob,DynamicSS(Tsit5()))
@@ -92,11 +92,10 @@ end
     from initial state u0
     and save output to file
 """
-function run_mlm_ss_from_init(u0, params, filename="default.txt")
+function run_mlm_ss_from_init(u0, params, dt=3600.0*5.0, tspan=3600.0*24.0*10.0, filename="default.txt")
     params.qft0 = calc_qft0(params.RHft, params.Gamma_q, params.sft0, params.Gamma_s);
 
     prob = SteadyStateProblem(mlm, u0, params);
-    tspan = 3600.0 * 24.0 * 60.0;
     tol = 1e-9;
 
     @time begin
@@ -104,9 +103,7 @@ function run_mlm_ss_from_init(u0, params, filename="default.txt")
         # sol = solve(prob, SSRootfind());
 
         println("euler");
-        mins = 10.0;
-        println("dt=",mins," mins")
-        sol = solve(prob, DynamicSS(Euler(); abstol=0.0, reltol=tol, tspan=tspan), dt=60.0*mins, progress=true, progress_steps=30);
+        sol = solve(prob, DynamicSS(Euler(); abstol=0.0, reltol=tol, tspan=tspan), dt=dt, progress=true, progress_steps=30);
 
         # println("tsit5");
         # sol = solve(prob, DynamicSS(Tsit5(); abstol=0.0, reltol=tol, tspan=tspan));

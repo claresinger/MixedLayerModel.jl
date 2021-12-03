50294aa3158017561b56f02d6ae933176eaed20d
claresinger <clareees@gmail.com>
2021-12-02 21:39:55 -0800

    plot inversion jump

--- experiments/co2_upstep.jl
+++ experiments/co2_upstep.jl
@@ -1,6 +1,7 @@
 push!(LOAD_PATH, joinpath(@__DIR__, ".."))
 
 using MixedLayerModel
+using MixedLayerModel: μ, L0
 using FileIO
 using Plots
 
@@ -8,11 +9,11 @@ include("mlm_solve_funcs.jl")
 
 # use command line argument to set co2
 # newCO2 = parse(Float64,ARGS[1]);
-newCO2 = 600.0;
+newCO2 = 2200.0;
 println(newCO2);
 
 # load initial condition from file
-path = "experiments/output/merge/";
+path = "experiments/output/correct_emissivity/";
 restarttry1 = path*"co2_upstep_"*string(Int(newCO2-100))*".jld2";
 restarttry2 = path*"co2_upstep_"*string(Int(newCO2-200))*".jld2";
 restarttry3 = path*"co2_upstep_"*string(Int(newCO2-400))*".jld2";
@@ -45,7 +46,7 @@ par.etype = enBal();
 par.fttype = co2dep();
 par.rtype = varRad();
 par.stype = varSST();
-dt, tmax = 4.0, 30;
+dt, tmax = 2.0, 30;
 
 println(par.OHU, "\t", par.R_s_400);
 
@@ -67,8 +68,8 @@ u0, sol = run_mlm_from_init(u0, par, dt=3600.0*dt, tspan=(0.0,3600.0*24.0*tmax))
 
 t = sol.t / 3600.0 / 24.0;
 zi = getindex.(sol.u,1);
-hM = getindex.(sol.u,2) * 1e-3;
-qtM = getindex.(sol.u,3) * 1e3;
+hM = getindex.(sol.u,2);
+qtM = getindex.(sol.u,3);
 sst = getindex.(sol.u,4);
 cf = getindex.(sol.u,5);
 S = zeros(length(t));
@@ -76,25 +77,31 @@ LHF = zeros(length(t));
 zb = zeros(length(t));
 ΔR = zeros(length(t));
 LWP = zeros(length(t));
+Δsvl = zeros(length(t));
+ent = zeros(length(t));
 for (i,si) in enumerate(S)
     zb[i] = calc_LCL(sol.u[i]);
     LWP[i] = incloud_LWP(sol.u[i], zb[i]);
     S[i] = calc_S(sol.u[i], par, zb[i], LWP[i]);
     LHF[i] = calc_LHF(sol.u[i], par);
     ΔR[i] = calc_cloudtop_RAD(sol.u[i], par, LWP[i], par.rtype);
+    Δsvl[i] = Δs(sol.u[i], par, zb[i]);
+    ent[i] = we(sol.u[i], par, zb[i], LWP[i], par.etype);
 end 
-plot(size=(1200,800), layout=(5,2), dpi=200, left_margin = 5Plots.mm);
+plot(size=(1200,800), layout=(6,2), dpi=200, left_margin = 5Plots.mm);
 plot!(t, zi, marker="o-", legend=false, subplot=1, ylabel="zi, zb [m]");
 plot!(t, zb, marker="o-", legend=false, subplot=1);
-plot!(t, hM, marker="o-", legend=false, subplot=2, ylabel="hM [kJ/kg]"); 
-plot!(t, qtM, marker="o-", legend=false, subplot=3, ylabel="qtM [g/kg]");
+plot!(t, hM * 1e-3, marker="o-", legend=false, subplot=2, ylabel="hM [kJ/kg]"); 
+plot!(t, qtM * 1e3, marker="o-", legend=false, subplot=3, ylabel="qtM [g/kg]");
 plot!(t, sst, marker="o-", legend=false, subplot=4, ylabel="SST [K]");
 plot!(t, cf * 1e2, marker="o-", legend=false, subplot=5, ylabel="CF [%]");
 plot!(t, LWP .* cf * 1e3, marker="o-", legend=false, subplot=6, ylabel="LWP [g/m2]");
-plot!(t, LHF, marker="o-", legend=false, subplot=7, ylabel="LHF [W/m2]");
-plot!(t, ΔR, marker="o-", legend=false, subplot=8, ylabel="ΔR [W/m2]");
-plot!(t, (zi .- zb) ./ zi, marker="o-", legend=false, subplot=9, ylabel="zc/zi [-]", xlabel="time [days]");
-plot!(t, S, marker="o-", legend=false, subplot=10, ylabel="S [-]", xlabel="time [days]");
+plot!(t, Δsvl * 1e-3, marker="o-", legend=false, subplot=7, ylabel="Δs (kJ/kg)");
+plot!(t, ent*1e3, marker="o-", legend=false, subplot=8, ylabel="we (mm/s)")
+plot!(t, LHF, marker="o-", legend=false, subplot=9, ylabel="LHF [W/m2]");
+plot!(t, ΔR, marker="o-", legend=false, subplot=10, ylabel="ΔR [W/m2]");
+plot!(t, (zi .- zb) ./ zi, marker="o-", legend=false, subplot=11, ylabel="zc/zi [-]", xlabel="time [days]");
+plot!(t, S, marker="o-", legend=false, subplot=12, ylabel="S [-]", xlabel="time [days]");
 mkpath(replace(path, "output"=>"figures"));
 savefig(replace(path, "output"=>"figures")*"sol"*string(Int(newCO2))*"_t.png");
 

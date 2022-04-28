4a34532c06b27c442afd0e2bfca36324d12bd48a
claresinger <clareees@gmail.com>
2022-04-27 17:55:26 -0700

    new experiment file to numerically do a linear stability analysis to calculate partial derivatives of the MLM with respect to various boundary conditions. this will be useful for the part 1 paper as well as maybe for the ese 134 homework assignment.

---
+++ experiments/linear_perturbations.jl
@@ -0,0 +1,61 @@
+push!(LOAD_PATH, joinpath(@__DIR__, ".."))
+
+using MixedLayerModel
+using FileIO
+using Plots
+
+include("mlm_solve_funcs.jl")
+
+SST_arr = [288, 289, 290, 291, 292];
+D_arr = [5.5, 5.75, 6, 6.25, 6.5]*1e-6;
+RHft_arr = [0.18, 0.19, 0.2, 0.21, 0.22];
+V_arr = [5,6,7,8,9];
+
+# X_arr = SST_arr;
+# X_label = "SST (K)"; 
+# X_arr = D_arr;
+# X_label = "D (1/s)";  
+# X_arr = V_arr;
+# X_label = "V (m/s)"; 
+# X_arr = RHft_arr;
+# X_label = "RHft";
+
+CF_arr = zeros(length(X_arr));
+LWP_arr = zeros(length(X_arr));
+for (i,Xi) in enumerate(X_arr)
+    # run simulation looping over X
+    par = climatology();
+    par.etype = bflux(); #fixed, enBal, bflux
+
+    par.SST0 = SST_arr[3] # (K), SST_arr[3]
+    par.D = D_arr[3] # (1/s), D_arr[3]
+    par.V = V_arr[3] # m/s, V_arr[3]
+    par.RHft = RHft_arr[3] # (-), RHft_arr[3]
+
+    par.Gamma_q = 0.0; # (kg/kg/m)
+    par.sft0 = 305; # (K)
+    par.Gamma_s = 0.0; # (K/m)
+
+    dt = 24.0;
+    tmax = 40.0;
+
+    ## plot time series
+    ENV["GKSwstype"]="nul"
+    u0, sol = run_mlm(par, dt=3600.0*dt, tspan=(0.0,3600.0*24.0*tmax));
+    uf = sol.u[end];
+    zi, hM, qM, SST, CFi = uf;
+    zb = calc_LCL(uf);
+    LWPi = incloud_LWP(uf, zb);
+    CF_arr[i] = CFi;
+    LWP_arr[i] = LWPi;
+end
+
+println(X_arr)
+println(CF_arr)
+println(LWP_arr*1e3)
+
+plot(size=(800,500), dpi=200, left_margin = 5Plots.mm);
+plot!(X_arr, LWP_arr*1e3, marker="o-", xlabel=X_label, ylabel="LWP (g/m2)", legend=false);
+path = "experiments/figures/linear_perturb/";
+mkpath(path);
+savefig(path*"lin_perturb_"*split(X_label, " (")[1]*".png");
